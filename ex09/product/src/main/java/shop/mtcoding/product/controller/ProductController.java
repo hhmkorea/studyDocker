c17e6ea5214e64b310073e3d788ed3214cbd1fee
hhmkorea <yuyee81@gmail.com>
2024-01-22 20:56:52 +0900

    Revert "31강"
    This reverts commit a3f806d16d5b8061ebf970c938cb9aed9e09d7b0.

--- ex09/product/src/main/java/shop/mtcoding/product/controller/ProductController.java
+++ ex09/product/src/main/java/shop/mtcoding/product/controller/ProductController.java
@@ -3,7 +3,6 @@ package shop.mtcoding.product.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import shop.mtcoding.product.model.Product;
--- ex09/product/src/test/java/shop/mtcoding/product/ProductApplicationTests.java
+++ ex09/product/src/test/java/shop/mtcoding/product/ProductApplicationTests.java
@@ -3,6 +3,7 @@ package shop.mtcoding.product;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class ProductApplicationTests {
 
 	@Test
