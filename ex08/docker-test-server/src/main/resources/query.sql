3f48b43e59af7a6e5fa624904346e9f2943e5205
hhmkorea <hhmkorea@nate.com>
2024-11-07 08:50:41 +0900

    mysql 기준으로 다시 실습.

--- ex08/docker-test-server/src/main/resources/query.sql
+++ ex08/docker-test-server/src/main/resources/query.sql
@@ -1,5 +1,12 @@
+
+create database metadb default character set utf8;
+show databases ;
+use metadb;
+
 CREATE TABLE user_tb (
                          id INT AUTO_INCREMENT PRIMARY KEY,
                          name VARCHAR(50)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 ;
+
+SELECT * FROM user_tb;
