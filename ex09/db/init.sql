4c1606fbafc09466659896d3289a5b2ae5bb1c81
hhmkorea <hhmkorea@nate.com>
2024-11-14 15:54:35 +0900

    도커컴포즈 react, springDB 포함 직접 연결하기

--- ex09/db/init.sql
+++ ex09/db/init.sql
@@ -6,3 +6,4 @@ CREATE TABLE product_tb (
 
 INSERT INTO product_tb (name, price) VALUES ('banana', 500);
 INSERT INTO product_tb (name, price) VALUES ('apple', 1000);
+INSERT INTO product_tb (name, price) VALUES ('melon', 2000);
--- ex09/product/src/test/java/shop/mtcoding/product/ProductApplicationTests.java
+++ ex09/product/src/test/java/shop/mtcoding/product/ProductApplicationTests.java
@@ -3,7 +3,7 @@ package shop.mtcoding.product;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ProductApplicationTests {
 
 	@Test
